9baf591bc92690224f20aca9a99d41f9ff2a8066
Luiz Carlos <sodebem2@gmail.com>
2025-04-16 12:20:35 -0300

    primeiro commit

---
+++ src/main/java/br/com/cadUser/Pessoa.java
@@ -0,0 +1,35 @@
+package br.com.cadUser;
+ 
+ public class Pessoa {
+     private String nome;
+     private String email;
+     private String telefone;
+ 
+     // Construtor vazio
+     public Pessoa() {}
+ 
+     // Getters e Setters
+     public String getNome() {
+         return nome;
+     }
+ 
+     public void setNome(String nome) {
+         this.nome = nome;
+     }
+ 
+     public String getEmail() {
+         return email;
+     }
+ 
+     public void setEmail(String email) {
+         this.email = email;
+     }
+ 
+     public String getTelefone() {
+         return telefone;
+     }
+ 
+     public void setTelefone(String telefone) {
+         this.telefone = telefone;
+     }
+ }
